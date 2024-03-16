0937b45962ff55d6257c8778858ba3a2ba81c3d1
Hallae <mr.tabby@hotmail.com>
2024-03-16 16:09:06 +0300

    Add project files.

---
+++ Controllers/StaffMembersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Members.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffMembersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var members = new List<StaffMembers>
+            {
+                new StaffMembers {id = 1,
+                    Address="komsomolsky dom 6",
+                    LastName =" Egor",
+                    FirstName =" ivan",
+                    Otchectva="egorvich",
+                    Salary= 60000 }
+  
+            };
+            return Ok(members);
+        }
+     
+    }
+}
---
+++ StaffMembers.cs
@@ -0,0 +1,19 @@
+﻿namespace Members
+{
+    public class StaffMembers
+    {
+        public int id {  get; set; }    
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string Otchectva { get; set; } = string.Empty;
+
+        public string LastName {  get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public int Salary { get; set; }
+
+
+    }
+}
