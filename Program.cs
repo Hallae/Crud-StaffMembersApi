271742df23732ef853ed7db851428f63bc01246a
Hallae <mr.tabby@hotmail.com>
2024-03-16 20:51:33 +0300

    Added DBcontext/EntityFramework

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+global using Members.Data;
+global using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add dbcontext for sqlserver
+builder.Services.AddDbContext<DataContext>(options =>
+{ options.UseSqlServer(); }
+) ;
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
